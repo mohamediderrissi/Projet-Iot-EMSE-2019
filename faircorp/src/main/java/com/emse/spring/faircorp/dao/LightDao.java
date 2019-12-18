124393524146c77de65413e06d4dcde062e8514e
mohamediderrissi <mohamediderrissi@gmail.com>
2019-12-18 08:56:03 +0100

    API Developped With Spring, To retrieve and manipulate light status in a room

---
+++ faircorp/src/main/java/com/emse/spring/faircorp/dao/LightDao.java
@@ -0,0 +1,9 @@
+package com.emse.spring.faircorp.dao;
+
+import com.emse.spring.faircorp.model.Light;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface LightDao extends JpaRepository<Light, Long> {
+
+}
